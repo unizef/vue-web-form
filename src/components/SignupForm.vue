b60e8a8dd23cb6ed60b313fc9026e4f7ec4f6027
unizef <zefstribos@gmail.com>
2021-04-10 11:41:52 +0200

    added keyup event on input field

--- src/components/SignupForm.vue
+++ src/components/SignupForm.vue
@@ -1,15 +1,68 @@
 <template>
   <div>
     <form>
+      <!-- Input Fields -->
       <label for="">Email:</label>
-      <input type="email" required />
+      <input type="email" required v-model="email" />
+
+      <label for="">Password:</label>
+      <input type="password" required v-model="password" />
+
+      <!-- Select Boxes -->
+      <label for="">Role:</label>
+      <select name="" id="" v-model="role">
+        <option value="developer">Web Developer</option>
+        <option value="designer">Web Designer</option>
+      </select>
+
+      <!-- Keyup Input Field -->
+      <label>Skill:</label>
+      <input type="text" v-model="tempSkill" @keyup.ctrl="addSkill" />
+      <div v-for="skill in skills" :key="skill" class="pill">
+        {{ skill }}
+      </div>
+
+      <!-- Check Boxes -->
+      <div class="terms">
+        <input type="checkbox" v-model="terms" required />
+        <label>Accept terms and conditions</label>
+      </div>
     </form>
+
+    <p>Your Email: {{ email }}</p>
+    <p>Your Password: {{ password }}</p>
+    <p>Role: {{ role }}</p>
+    <p>Terms: {{ terms }}</p>
+    <p>Names: {{ names }}</p>
+    <p>Skills: {{ skills }}</p>
   </div>
 </template>
 
 <script>
 import "../assets/styles/global.css";
-export default {};
+export default {
+  data() {
+    return {
+      email: "",
+      password: "secred123",
+      role: "",
+      terms: false,
+      names: [],
+      tempSkill: "",
+      skills: [],
+    };
+  },
+  methods: {
+    addSkill(e) {
+      if (e.key === "," || (e.key === ";" && this.tempSkill)) {
+        if (!this.skills.includes(this.tempSkill)) {
+          this.skills.push(this.tempSkill);
+        }
+        this.tempSkill = "";
+      }
+    },
+  },
+};
 </script>
 
 <style>
@@ -32,12 +85,21 @@ label {
   font-weight: bold;
 }
 
-input {
+input,
+select {
   display: block;
   padding: 10px 6px;
   width: 100%;
   border: none;
   border-bottom: 1px solid #ddd;
   color: #555;
+}
+
+input[type="checkbox"] {
+  display: inline-block;
+  width: 16px;
+  margin: 0 10px 0 0;
+  position: relative;
+  top: 3px;
 }
 </style>
